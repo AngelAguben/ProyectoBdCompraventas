d27f46c1bea166f4c01084f1d7e0cba51dde9430
Angel <angelbmx002@gmail.com>
2021-05-22 19:35:31 +0200

    Cambia que los switches ahora están dentro del que deberían para que podamos repetir la ejecución varias veces hasta que el usuario decida salir en el menú principal

--- src/main/java/appfinal/Aplicacion.java
+++ src/main/java/appfinal/Aplicacion.java
@@ -53,200 +53,191 @@ public class Aplicacion {
 						System.out.println("Lista de vehículos: ");
 						imprimirListaCoches(catalogoCoches);
 						System.out.println("¿Qué desea hacer? \n" + "1. Crear \n" + "2. Borrar \n" + "3. Buscar \n"
-								+ "4. Modificar \n" + "5. Alquilar \n" + "6. Salir");
+								+ "4. Modificar \n" + "5. Salir");
 						opcionMenu = teclado.nextInt();
 						// Valores válidos entre 1-6, si no se repetirá el bucle
-					} while (opcionMenu < 1 || opcionMenu > 6);
+					} while (opcionMenu < 1 || opcionMenu > 5);
 
 					// Switch de las opciones del Menu
 					switch (opcionMenu) {
-					case 1: // REGISTAR
+					case 1: // CREAR
 						do {
-							System.out.println(
-									"¿Qué desea registar? \n" + "1. Crear Aprendiz \n" + "2. Crear Trabajador \n"
-											+ "3. Crear Cliente \n" + "4. Crear Coche \n" + "5. Crear Deportivo \n"
-											+ "6. Crear SUV \n" + "7. Crear ContratoCompra \n" + "8. Salir");
-							opCrear = teclado.nextInt();
-							// Valores válidos entre 1-8, si no se repetirá el bucle
-						} while (opCrear < 1 || opCrear > 8);
+							do {
+								System.out.println(
+										"¿Qué desea registar? \n" + "1. Crear Aprendiz \n" + "2. Crear Trabajador \n"
+												+ "3. Crear Cliente \n" + "4. Crear Coche \n" + "5. Crear Deportivo \n"
+												+ "6. Crear SUV \n" + "7. Crear ContratoCompra \n" + "8. Salir");
+								opCrear = teclado.nextInt();
+								// Valores válidos entre 1-8, si no se repetirá el bucle
+							} while (opCrear < 1 || opCrear > 8);
+
+							// Switch de las opciones de Crear
+							switch (opCrear) {
+							// CASOS
+							case 1: // Aprendiz
+								cAprendiz.createAprendiz(ClaseCrear.insertarDatosAprendiz());
+								break;
+							case 2: // Trabajador
+								cTrabajador.createTrabajador(ClaseCrear.insertarDatosTrabajador());
+								break;
+							case 3: // Cliente
+								cCliente.createCliente(ClaseCrear.insertarDatosCliente());
+								break;
+							case 4: // Coche
+								cCoche.createCoche(ClaseCrear.insertarDatosCoche());
+								break;
+							case 5: // Deportivo
+								cDeportivo.createDeportivo(ClaseCrear.insertarDatosDeportivo());
+								break;
+							case 6: // Suv
+								cSuv.createSuv(ClaseCrear.insertarDatosSuv());
+								break;
+							case 7: // ContratoCompra
+//								cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
+								break;
+							case 8: // SALIR
+								System.out.println("Saliendo del menu");
+								break;
+							}
+						} while (opCrear != 8);
 						break;
 
 					case 2: // BORRAR
 						do {
-							System.out.println(
-									"¿Qué desea borrar? \n" + "1. Borrar Aprendiz \n" + "2. Borrar Trabajador \n"
-											+ "3. Borrar Cliente \n" + "4. Borrar Coche \n" + "5. Borrar Deportivo \n"
-											+ "6. Borrar Suv \n" + "7. Borrar ContratoCompra \n" + "8. Salir");
-							opBorrar = teclado.nextInt();
-							// Valores válidos entre 1-8, si no se repetirá el bucle
-						} while (opBorrar < 1 || opBorrar > 8);
+							do {
+								System.out.println("¿Qué desea borrar? \n" + "1. Borrar Aprendiz \n"
+										+ "2. Borrar Trabajador \n" + "3. Borrar Cliente \n" + "4. Borrar Coche \n"
+										+ "5. Borrar Deportivo \n" + "6. Borrar Suv \n" + "7. Borrar ContratoCompra \n"
+										+ "8. Salir");
+								opBorrar = teclado.nextInt();
+								// Valores válidos entre 1-8, si no se repetirá el bucle
+							} while (opBorrar < 1 || opBorrar > 8);
+
+							System.out.println("--- BORRAR ---");
+							System.out.println("Introduzca el código a borrar: ");
+							// Switch de las opciones de Borrar
+							switch (opBorrar) {
+							// CASOS
+							case 1: // Aprendiz
+								cAprendiz.borrarAprendiz(cAprendiz.findByPK(teclado.nextInt()));
+								break;
+							case 2: // Trabajador
+								cTrabajador.borrarTrabajador(cTrabajador.findByPK(teclado.nextInt()));
+								break;
+							case 3: // Cliente
+								cCliente.borrarCliente(cCliente.findByPK(teclado.nextInt()));
+								break;
+							case 4: // Coche
+								cCoche.borrarCoche(cCoche.findByPK(teclado.nextInt()));
+								break;
+							case 5: // Deportivo
+								cDeportivo.borrarDeportivo(cDeportivo.findByPK(teclado.nextInt()));
+								break;
+							case 6: // Suv
+								cSuv.borrarSuv(cSuv.findByPK(teclado.nextInt()));
+								break;
+							case 7: // ContratoCompra
+//								cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
+								break;
+							case 8: // SALIR
+								System.out.println("Saliendo del menu");
+								break;
+							}
+						} while (opBorrar != 8);
 						break;
 
 					case 3: // BUSCAR
 						do {
-							System.out.println(
-									"¿Qué desea buscar? \n" + "1. Buscar Aprendiz \n" + "2. Buscar Trabajador \n"
-											+ "3. Buscar Cliente \n" + "4. Buscar Coche \n" + "5. Buscar Deportivo \n"
-											+ "6. Buscar Suv \n" + "7. Buscar ContratoCompra \n" + "8. Salir");
-							opBuscar = teclado.nextInt();
-							// Valores válidos entre 1-8, si no se repetirá el bucle
-						} while (opBuscar < 1 || opBuscar > 8);
+							do {
+								System.out.println("¿Qué desea buscar? \n" + "1. Buscar Aprendiz \n"
+										+ "2. Buscar Trabajador \n" + "3. Buscar Cliente \n" + "4. Buscar Coche \n"
+										+ "5. Buscar Deportivo \n" + "6. Buscar Suv \n" + "7. Buscar ContratoCompra \n"
+										+ "8. Salir");
+								opBuscar = teclado.nextInt();
+								// Valores válidos entre 1-8, si no se repetirá el bucle
+							} while (opBuscar < 1 || opBuscar > 8);
+
+							System.out.println("--- BUSCAR ---");
+							System.out.println("Introduzca el código a buscar: ");
+							// Switch de las opciones de Buscar
+							switch (opBuscar) {
+							// CASOS
+							case 1: // Aprendiz
+								cAprendiz.findByPK(teclado.nextInt());
+								break;
+							case 2: // Trabajador
+								cTrabajador.findByPK(teclado.nextInt());
+								break;
+							case 3: // Cliente
+								cCliente.findByPK(teclado.nextInt());
+								break;
+							case 4: // Coche
+								cCoche.findByPK(teclado.nextInt());
+								break;
+							case 5: // Deportivo
+								cDeportivo.findByPK(teclado.nextInt());
+								break;
+							case 6: // Suv
+								cSuv.findByPK(teclado.nextInt());
+								break;
+							case 7: // ContratoCompra
+//								cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
+								break;
+							case 8: // SALIR
+								System.out.println("Saliendo del menu");
+								break;
+							}
+						} while (opBuscar != 8);
 						break;
 
 					case 4: // MODIFICAR
 						do {
-							System.out.println("¿Qué desea modificar? \n" + "1. Modificar Aprendiz \n"
-									+ "2. Modificar Trabajador \n" + "3. Modificar Cliente \n" + "4. Modificar Coche \n"
-									+ "5. Modificar Deportivo \n" + "6. Modificar Suv \n"
-									+ "7. Modificar ContratoCompra \n" + "8. Salir");
-							opMod = teclado.nextInt();
-							// Valores válidos entre 1-8, si no se repetirá el bucle
-						} while (opMod < 1 || opMod > 8);
+							do {
+								System.out.println("¿Qué desea modificar? \n" + "1. Modificar Aprendiz \n"
+										+ "2. Modificar Trabajador \n" + "3. Modificar Cliente \n"
+										+ "4. Modificar Coche \n" + "5. Modificar Deportivo \n" + "6. Modificar Suv \n"
+										+ "7. Modificar ContratoCompra \n" + "8. Salir");
+								opMod = teclado.nextInt();
+								// Valores válidos entre 1-8, si no se repetirá el bucle
+							} while (opMod < 1 || opMod > 8);
+
+							System.out.println("--- MODIFICAR ---");
+							System.out.println("Introduzca el código a modificar: ");
+							// Switch de las opciones de Modificar
+							switch (opMod) {
+							// CASOS
+							case 1: // Aprendiz
+								cAprendiz.modifyAprendiz(ClaseCrear.insertarDatosAprendiz());
+								break;
+							case 2: // Trabajador
+								cTrabajador.modifyTrabajador(ClaseCrear.insertarDatosTrabajador());
+								break;
+							case 3: // Cliente
+								cCliente.modifyCliente(ClaseCrear.insertarDatosCliente());
+								break;
+							case 4: // Coche
+								cCoche.modifyCoche(ClaseCrear.insertarDatosCoche());
+								break;
+							case 5: // Deportivo
+								cDeportivo.modifyDeportivo(ClaseCrear.insertarDatosDeportivo());
+								break;
+							case 6: // Suv
+								cSuv.modifySuv(ClaseCrear.insertarDatosSuv());
+								break;
+							case 7: // ContratoCompra
+//							cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
+								break;
+							case 8: // SALIR
+								System.out.println("Saliendo del menu");
+								break;
+							}
+						} while (opMod != 8);
 						break;
 
 					case 5: // SALIR
 						System.out.println("Saliendo del programa...");
 						fin = true;
 						break;
-					}
-
-					// Si opBuscar tiene contenido ejecutará el código
-					if (opCrear != 0) {
-						System.out.println("--- CREAR ---");
-						// Switch de las opciones de Crear
-						switch (opCrear) {
-						// CASOS
-						case 1: // Aprendiz
-							cAprendiz.createAprendiz(ClaseCrear.insertarDatosAprendiz());
-							break;
-						case 2: // Trabajador
-							cTrabajador.createTrabajador(ClaseCrear.insertarDatosTrabajador());
-							break;
-						case 3: // Cliente
-							cCliente.createCliente(ClaseCrear.insertarDatosCliente());
-							break;
-						case 4: // Coche
-							cCoche.createCoche(ClaseCrear.insertarDatosCoche());
-							break;
-						case 5: // Deportivo
-							cDeportivo.createDeportivo(ClaseCrear.insertarDatosDeportivo());
-							break;
-						case 6: // Suv
-							cSuv.createSuv(ClaseCrear.insertarDatosSuv());
-							break;
-						case 7: // ContratoCompra
-//							cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
-							break;
-						case 8: // SALIR
-							System.out.println("Saliendo del programa");
-							fin = true;
-							break;
-						}
-					}
-
-					// Si opBorrar tiene contenido ejecutará el código
-					if (opBorrar != 0) {
-						System.out.println("--- BORRAR ---");
-						System.out.println("Introduzca el código a borrar: ");
-						// Switch de las opciones de Borrar
-						switch (opBorrar) {
-						// CASOS
-						case 1: // Aprendiz
-							cAprendiz.borrarAprendiz(cAprendiz.findByPK(teclado.nextInt()));
-							break;
-						case 2: // Trabajador
-							cTrabajador.borrarTrabajador(cTrabajador.findByPK(teclado.nextInt()));
-							break;
-						case 3: // Cliente
-							cCliente.borrarCliente(cCliente.findByPK(teclado.nextInt()));
-							break;
-						case 4: // Coche
-							cCoche.borrarCoche(cCoche.findByPK(teclado.nextInt()));
-							break;
-						case 5: // Deportivo
-							cDeportivo.borrarDeportivo(cDeportivo.findByPK(teclado.nextInt()));
-							break;
-						case 6: // Suv
-							cSuv.borrarSuv(cSuv.findByPK(teclado.nextInt()));
-							break;
-						case 7: // ContratoCompra
-//							cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
-							break;
-						case 8: // SALIR
-							System.out.println("Saliendo del programa");
-							fin = true;
-							break;
-						}
-					}
-
-					// Si opBuscar tiene contenido ejecutará el código
-					if (opBuscar != 0) {
-						System.out.println("--- BUSCAR ---");
-						System.out.println("Introduzca el código a buscar: ");
-						// Switch de las opciones de Buscar
-						switch (opBuscar) {
-						// CASOS
-						case 1: // Aprendiz
-							cAprendiz.findByPK(teclado.nextInt());
-							break;
-						case 2: // Trabajador
-							cTrabajador.findByPK(teclado.nextInt());
-							break;
-						case 3: // Cliente
-							cCliente.findByPK(teclado.nextInt());
-							break;
-						case 4: // Coche
-							cCoche.findByPK(teclado.nextInt());
-							break;
-						case 5: // Deportivo
-							cDeportivo.findByPK(teclado.nextInt());
-							break;
-						case 6: // Suv
-							cSuv.findByPK(teclado.nextInt());
-							break;
-						case 7: // ContratoCompra
-//							cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
-							break;
-						case 8: // SALIR
-							System.out.println("Saliendo del programa");
-							fin = true;
-							break;
-						}
-					}
-
-					// Si opMod tiene contenido ejecutará el código
-					if (opMod != 0) {
-						System.out.println("--- MODIFICAR ---");
-						System.out.println("Introduzca el código a modificar: ");
-						// Switch de las opciones de Modificar
-						switch (opMod) {
-						// CASOS
-						case 1: // Aprendiz
-							cAprendiz.modifyAprendiz(ClaseCrear.insertarDatosAprendiz());
-							break;
-						case 2: // Trabajador
-							cTrabajador.modifyTrabajador(ClaseCrear.insertarDatosTrabajador());
-							break;
-						case 3: // Cliente
-							cCliente.modifyCliente(ClaseCrear.insertarDatosCliente());
-							break;
-						case 4: // Coche
-							cCoche.modifyCoche(ClaseCrear.insertarDatosCoche());
-							break;
-						case 5: // Deportivo
-							cDeportivo.modifyDeportivo(ClaseCrear.insertarDatosDeportivo());
-							break;
-						case 6: // Suv
-							cSuv.modifySuv(ClaseCrear.insertarDatosSuv());
-							break;
-						case 7: // ContratoCompra
-//							cContratoCompra.createContratoCompra(ClaseCrear.insertarDatosContratoCompra());
-							break;
-						case 8: // SALIR
-							System.out.println("Saliendo del programa");
-							fin = true;
-							break;
-						}
 					}
 
 					// Control del error de que la base de datos no arroje ningún resultado
@@ -271,7 +262,7 @@ public class Aplicacion {
 			// ya que si hay un error de una excepción anterior se pondrá a true la
 			// variable error y si fin es false significará que no ha seleccionado
 			// la opción de salir
-		} while (error == true && fin == false);
+		} while (error == true || opcionMenu != 5);
 //		fin = true;
 		// Muestro por pantalla para saber cuando termina el programa
 		System.out.println("----- LA EJECUCIÓN DEL PROGRAMA HA TERMINADO -----");
--- src/main/java/appfinal/ClaseCrear.java
+++ src/main/java/appfinal/ClaseCrear.java
@@ -43,32 +43,6 @@ public class ClaseCrear {
 		}
 		return aprendiz;
 	}
-
-	// Método para crear un aprendiz entero
-//	public static void crearAprendizEntero() {
-//		// Se crea un nuevo controlador para poder trabajar con el
-//		ControladorAprendiz ca = new ControladorAprendiz();
-//		// También se crea controlador de trabajador para su uso como fk
-//		ControladorTrabajador ct = new ControladorTrabajador();
-//		// Creamos un nuevo aprendiz para insertarle los datos necesarios para después
-//		// pasarlo por el método de creación
-//		Aprendiz aprendiz = new Aprendiz();
-//		try {
-//			System.out.println("APRENDIZ");
-//			System.out.println("Nombre");
-//			aprendiz.setNomapren(teclado.nextLine());
-//			System.out.println("Número teléfono");
-//			aprendiz.setTlfnoapren(teclado.nextLine());
-//			System.out.println("Dni");
-//			aprendiz.setDniapren(teclado.nextLine());
-//			System.out.println("¿A cargo de que trabajador está? (Inserte su código por favor)");
-//			aprendiz.setTrabajador(ct.findByPK(teclado.nextInt()));
-//			// Y luego se crea el objeto
-//			ca.createAprendiz(aprendiz);
-//		} catch (InputMismatchException ime) {
-//			System.out.println("El dato no es el que se pide");
-//		}
-//	}
 	
 	// Método para insertar los datos de Trabajador
 	public static Trabajador insertarDatosTrabajador() {
@@ -89,31 +63,6 @@ public class ClaseCrear {
 		}
 		return trabajador;
 	}
-
-	// Método para crear un aprendiz entero
-//	public static void crearTrabajadorEntero() {
-//		// Se crea un nuevo controlador para poder trabajar con el
-//		ControladorTrabajador ct = new ControladorTrabajador();
-//		// Creamos un nuevo trabajador para insertarle los datos necesarios para después
-//		// pasarlo por el método de creación
-//		Trabajador trabajador = new Trabajador();
-//		try {
-//			System.out.println("Trabajador");
-//			System.out.println("Nombre");
-//			trabajador.setNomtrab(teclado.nextLine());
-//			System.out.println("Dni");
-//			trabajador.setDnitrab(teclado.nextLine());
-//			System.out.println("Horario");
-//			trabajador.setHorario(teclado.nextLine());
-//			System.out.println("Número teléfono");
-//			trabajador.setTlfnotrab(teclado.nextLine());
-//
-//			// Y luego se crea el objeto
-//			ct.createTrabajador(trabajador);
-//		} catch (InputMismatchException ime) {
-//			System.out.println("El dato no es el que se pide");
-//		}
-//	}
 	
 	// Método para insertar los datos de Cliente
 	public static Cliente insertarDatosCliente() {
@@ -141,37 +90,6 @@ public class ClaseCrear {
 		return cliente;
 	}
 
-	// Método para crear un cliente entero
-//	public static void crearClienteEntero() {
-//		// Se crea un nuevo controlador para poder trabajar con el
-//		ControladorCliente cc = new ControladorCliente();
-//		// Creamos un nuevo cliente para insertarle los datos necesarios para después
-//		// pasarlo por el método de creación
-//		Cliente cliente = new Cliente();
-//		try {
-//			System.out.println("CLIENTE");
-//			System.out.println("Nombre");
-//			cliente.setNomclien(teclado.nextLine());
-//			System.out.println("Apellido 1");
-//			cliente.setApe1clien(teclado.nextLine());
-//			System.out.println("Apellido 2");
-//			cliente.setApe2clien(teclado.nextLine());
-//			System.out.println("Número teléfono");
-//			cliente.setTlfnoclien(teclado.nextLine());
-//			System.out.println("Correo Electronico");
-//			cliente.setCorreoclien(teclado.nextLine());
-//			System.out.println("Usuario");
-//			cliente.setUsuclien(teclado.nextLine());
-//			System.out.println("Contraseña");
-//			cliente.setContraclien(teclado.nextLine());
-//
-//			// Y luego se crea el objeto
-//			cc.createCliente(cliente);
-//		} catch (InputMismatchException ime) {
-//			System.out.println("El dato no es el que se pide");
-//		}
-//	}
-	
 	// Método para insertar los datos de Coche
 	public static Coche insertarDatosCoche() {
 		Coche coche = new Coche();
@@ -211,49 +129,6 @@ public class ClaseCrear {
 		}
 		return coche;
 	}
-
-//	public static void crearCocheEntero() {
-//		// Se crea un nuevo controlador para poder trabajar con el
-//		ControladorCoche cc = new ControladorCoche();
-//		// Creamos un nuevo coche para insertarle los datos necesarios para después
-//		// pasarlo por el método de creación
-//		Coche coche = new Coche();
-//		// He creado un String para guardar la respuesta del usuario
-//		String textoRespu = "";
-//
-//		try {
-//			System.out.println("COCHE");
-//			System.out.println("Matricula");
-//			coche.setMatricula(teclado.nextLine());
-//			do {
-//				System.out.println("Cambio (Manual/Automatico)");
-//				textoRespu = teclado.nextLine();
-//				// Si es manual
-//				if (textoRespu.equalsIgnoreCase("manual")) {
-//					coche.setCambio(false);
-//					// Si no...
-//				} else if (textoRespu.equalsIgnoreCase("automatico")) {
-//					// Será true
-//					coche.setCambio(true);
-//				}
-//			} while (!textoRespu.equalsIgnoreCase("manual") && !textoRespu.equalsIgnoreCase("automatico"));
-//
-//			System.out.println("Color");
-//			coche.setColor(teclado.nextLine());
-//			System.out.println("Marca");
-//			coche.setMarca(teclado.nextLine());
-//			System.out.println("Modelo");
-//			coche.setModelo(teclado.nextLine());
-//			System.out.println("Cv");
-//			coche.setCv(teclado.nextLine());
-//			System.out.println("Precio");
-//			coche.setPrecio(teclado.nextDouble());
-//			// Y luego se crea el objeto
-//			cc.createCoche(coche);
-//		} catch (InputMismatchException ime) {
-//			System.out.println("El dato no es el que se pide");
-//		}
-//	}
 
 	// Método para insertar los datos de Deportivo
 	public static Deportivo insertarDatosDeportivo() {
@@ -312,63 +187,6 @@ public class ClaseCrear {
 		return depor;
 	}
 
-//	public static void crearDeportivoEntero() {
-//		// Se crea un nuevo controlador para poder trabajar con el
-//		ControladorDeportivo cd = new ControladorDeportivo();
-//		// También se crea controlador de Coche para su uso como fk
-//		ControladorCoche cc = new ControladorCoche();
-//		// Creamos un nuevo deportivo para insertarle los datos necesarios para después
-//		// pasarlo por el método de creación
-//		Deportivo depor = new Deportivo();
-//		// He creado un String para guardar la respuesta del usuario
-//		String textoRespu = "";
-//
-//		try {
-//			try {
-//				System.out.println("DEPORTIVO");
-//				System.out.println("Dígame el código del coche");
-//				depor.setCoche(cc.findByPK(teclado.nextInt()));
-//
-//				do {
-//					System.out.println("¿El vehículo monta pack aero? (si/no)");
-//					textoRespu = teclado.nextLine();
-//					// Si es si...
-//					if (textoRespu.equalsIgnoreCase("si")) {
-//						// Será true
-//						depor.setPackaero(true);
-//						// Si no...
-//					} else if (textoRespu.equalsIgnoreCase("no")) {
-//						// Será false
-//						depor.setPackaero(false);
-//					}
-//
-//				} while (!textoRespu.equalsIgnoreCase("no") && !textoRespu.equalsIgnoreCase("si"));
-//
-//				// Repite hasta que sea si o no la respuesta
-//				do {
-//					System.out.println("¿El vehículo monta turbo? (si/no)");
-//					textoRespu = teclado.nextLine();
-//					// Si es si...
-//					if (textoRespu.equalsIgnoreCase("si")) {
-//						// Será true
-//						depor.setTurbo(true);
-//						// Si no...
-//					} else if (textoRespu.equalsIgnoreCase("no")) {
-//						// Será false
-//						depor.setTurbo(false);
-//					}
-//
-//				} while (!textoRespu.equalsIgnoreCase("no") && !textoRespu.equalsIgnoreCase("si"));
-//				// Y luego se crea el objeto
-//				cd.createDeportivo(depor);
-//			} catch (NoResultException nre) {
-//				System.out.println("Dicho valor no se encuentra en la base de datos");
-//			}
-//		} catch (InputMismatchException ime) {
-//			System.out.println("El dato no es el que se pide");
-//		}
-//	}
-
 	// Método para insertar los datos de Suv
 	public static Suv insertarDatosSuv() {
 		ControladorSuv cd = new ControladorSuv();
@@ -399,37 +217,5 @@ public class ClaseCrear {
 		}
 		return suv;
 	}
-
-//	public static void crearSuvEntero() {
-//		ControladorSuv cd = new ControladorSuv();
-//		// También se crea controlador de Coche para su uso como fk
-//		ControladorCoche cc = new ControladorCoche();
-//		// Creamos un nuevo suv para insertarle los datos necesarios para después
-//		// pasarlo por el método de creación
-//		Suv s1 = new Suv();
-//
-//		int respu = 0;
-//
-//		try {
-//			try {
-//				System.out.println("SUV");
-//				System.out.println("Dígame el código del coche");
-//				s1.setCoche(cc.findByPK(teclado.nextInt()));
-//				System.out.println("¿Cúantas plazas tiene el vehículo?");
-//				// Repite mientras que el numero de plazas sea menor a 1 o mayor a 10
-//				do {
-//					respu = teclado.nextInt();
-//				} while (respu < 1 && respu > 10);
-//				s1.setPlazas(respu);
-//
-//				// Y luego se crea el objeto
-//				cd.createSuv(s1);
-//			} catch (NoResultException nre) {
-//				System.out.println("Dicho valor no se encuentra en la base de datos");
-//			}
-//		} catch (InputMismatchException ime) {
-//			System.out.println("El dato no es el que se pide");
-//		}
-//	}
 
 }
